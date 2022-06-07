b559d314c83ca974e38a7ed7cd94e65c545f5c5b
Elvino03 <102396151+Elvino03@users.noreply.github.com>
2022-06-07 01:38:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ function compute()
     var rate = document.getElementById("rate").value; //This declares the variable "rate"
     var years = document.getElementById("years").value; //This declares the variable "years"
     var interest = principal * years * rate /100; //This declares the variable "interest" which is the product of the principal,rate and years divided by 100
-    var amount = interest + principal; //This declares the variable "amount" which is the sum of the prinicipal and the interest
     console.log(interest) // This prints the interest
     var year = new Date().getFullYear()+parseInt(years); //This gives the code to get the year one gets the amount 
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>.\<br\>" // This prints out default messages depending on the input given
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>.\<br\>" // This prints out default messages depending on the input given
 }
         
 function updateRate() // This is used to update the rate as it is being moved or changed
